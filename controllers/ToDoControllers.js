c0a379b6249cd24c67662e72ead72d05702dabba
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-08-06 06:11:25 +0530

    create todo.ejs file and update style and todocontroller

--- controllers/ToDoControllers.js
+++ controllers/ToDoControllers.js
@@ -1,7 +1,8 @@
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding'}]
 module.exports = function(app){
 
     app.get('/todo', function(req, res){
-
+        res.render('todo', {todos: data});
     });
     app.post('/todo', function(req, res){
 
